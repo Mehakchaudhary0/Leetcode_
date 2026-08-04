c2f09a2fc9374cf25b37ec2983104f726063b2f1
Mehakkkkk <its.mehak.official@gmail.com>
2026-08-04 09:46:49 +0530

    Time: 12 ms (91.19%), Space: 26 MB (18.8%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,13 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        seen = set()
-
-        for num in nums:
-            if num in seen:
-                return True
-
-            seen.add(num)
-
-        return False
+        return len(nums) != len(set(nums))
         
