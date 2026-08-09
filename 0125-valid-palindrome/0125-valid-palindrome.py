6cebd3c9f2169ba6bbd8f2060a98374611a6decf
Mehakkkkk <its.mehak.official@gmail.com>
2026-08-09 23:16:32 +0530

    Time: 7 ms (98.52%), Space: 13.5 MB (27.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,20 +4,5 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        left = 0
-        right = len(s) - 1
-
-        while left < right:
-            while left < right and not s[left].isalnum():
-                left += 1
-
-            while left < right and not s[right].isalnum():
-                right -= 1
-
-            if s[left].lower() != s[right].lower():
-                return False
-
-            left += 1
-            right -= 1
-
-        return True
+        s = ''.join(e for e in s if e.isalnum()).lower()
+        return s==s[::-1]
